bd04dcd65b0281d1699fe0017a7837bc9b9a6d82
Kiv <marchfori@users.noreply.github.com>
2019-12-04 23:48:32 -0600

    organization

---
+++ code/StatusIndicator.cs
@@ -0,0 +1,25 @@
+﻿//player's status bar
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform HealthBar;
+
+    void Start()
+    {
+        if (HealthBar == null)
+        {
+            Debug.LogError("No healthbar reference");
+        }
+    }
+
+    //player health bar
+    public void setHealth(int Current, int Max)
+    {
+        float Value = (float) Current / Max;
+
+        HealthBar.localScale = new Vector3(Value, HealthBar.localScale.y, HealthBar.localScale.z);
+    }
+}
